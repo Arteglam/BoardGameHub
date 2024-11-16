05eba5529cff7b0bc5781580088d2299458fec69
ArmorDude <legend.ivanov@gmail.com>
2024-11-16 22:03:23 +0200

    Sample Register component and styles

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-
+  onSubmit() {
+    // Do something
+  }
 }
