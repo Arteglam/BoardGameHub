a1a3e4a7349cd2e0c75ddb182c9eb5743a99f8d8
ArmorDude <legend.ivanov@gmail.com>
2024-11-16 19:57:39 +0200

    Initialize the project and make structure folders

---
+++ src/app/games/games-catalog/games-catalog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games-catalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './games-catalog.component.html',
+  styleUrl: './games-catalog.component.scss'
+})
+export class GamesCatalogComponent {
+
+}
