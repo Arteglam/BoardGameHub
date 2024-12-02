a9b7fe16ef4ac13dc582142eb18b7b9ad5e49d15
ArmorDude <legend.ivanov@gmail.com>
2024-12-02 22:23:26 +0200

    Update Firebase configuration for hosting and environment settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,7 +7,7 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { getStorage, provideStorage } from '@angular/fire/storage';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.production';
 
 
 export const appConfig: ApplicationConfig = {
